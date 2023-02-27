0b53dc96c4a8bcf611fbbd105fa8eda172428d5a
zaranggi <donnyirianto.anggriawan@gmail.com>
2023-02-27 16:51:56 +0700

    update pm2 ecosystem files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps : [{
-      name: "pbro_reg1",
+      name: "lisdc",
       script: "index.js", 
       autorestart: true,
-      max_memory_restart: "500M", 
+      max_memory_restart: "1000M", 
       exec_mode  : "fork"
     },
   ], 
