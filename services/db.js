fffad635092dc4be407d0da327acc0f083de7aa9
zaranggi <donnyirianto.anggriawan@gmail.com>
2024-03-26 10:09:14 +0700

    update

--- services/db.js
+++ services/db.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 async function query(query) {
   try {
-    const result = await axios.post('http://192.168.131.18:7272/api/v1/mysql/query', {query : query}) 
+    const result = await axios.post('http://192.168.131.71:7272/api/v1/mysql/query', {query : query}) 
     return result.data.data[0]
   } catch (error) {   
     throw error
